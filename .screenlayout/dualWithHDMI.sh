3c68a13e5a4ef728db2f279abc4503b672b3776b
makerlik <murtazamakasaadi@gmail.com>
2022-06-16 15:28:58 +0000

    installing packages

--- .screenlayout/dualWithHDMI.sh
+++ .screenlayout/dualWithHDMI.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output eDP-1-1 --mode 1366x768 --pos 1920x0 --rotate normal --output VGA-1-1 --off --output HDMI-1-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+xrandr --output eDP1 --mode 1366x768 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VGA1 --off --output VIRTUAL1 --off

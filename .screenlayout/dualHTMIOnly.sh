0cf0568c935f78ee78f3a615a336948bfeb60b52
Murtaza Asaadi <mergenelos@gmail.com>
2025-08-04 13:18:40 -0400

    Delete .screenlayout directory

--- .screenlayout/dualHTMIOnly.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output DP-0 --off --output DP-1 --off --output DP-2 --off --output HDMI-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output eDP-1-1 --off --output DP-1-1 --off --output DP-1-2 --off --output DP-1-3 --off --output DP-1-4 --off --output DP-1-5 --off
